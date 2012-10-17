a534a48ab8e53ec4fdb1fe81ca215cb985f8606c
= <=>
2012-10-17 20:22:26 +0800

    +1

---
+++ define.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   define.h
+ * Author: xiaose
+ *
+ * Created on 2012年10月17日, 下午5:14
+ */
+
+#ifndef DEFINE_H
+#define	DEFINE_H
+#define INTSIZE 4
+#define NAMESIZE 20
+#include <iostream>
+
+
+enum DataType //枚举数据类型
+{
+    INT, CHAR, DOUBLE
+};
+
+#endif	/* DEFINE_H */
+
